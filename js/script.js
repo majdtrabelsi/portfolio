1786fa62c22801d8e13a35c1493ff452c0d39cd9
majdtrabelsi <majdtrabelsi2003@gmail.com>
2025-12-17 13:38:44 +0100

    SYSTÈME DE TRADUCTION EN / FR

--- js/script.js
+++ js/script.js
@@ -78,3 +78,86 @@ darkToggle.onclick = () => {
         darkToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
     }
 };
+
+
+/* =========================
+   LANGUAGE SWITCH (EN / FR)
+========================= */
+
+const translations = {
+    en: {
+        home: "Home",
+        about: "About",
+        services: "Services",
+        portfolio: "Portfolio",
+
+        hello: "Hello, It's Me",
+        role: "And I'm a Full Stack Developer",
+        home_desc: "I design and develop full-stack applications — from dynamic front-end interfaces to robust back-end systems.",
+
+        about_title: "About Me",
+        about_desc: "I’m a passionate Full-Stack Developer who loves building modern, efficient, and user-friendly digital experiences.",
+
+        download_cv: "Download CV",
+        about_dev:"Full Stack Developer"
+    },
+
+    fr: {
+        home: "Accueil",
+        about: "À propos",
+        services: "Services",
+        portfolio: "Portfolio",
+
+        hello: "Bonjour, c’est moi",
+        role: "Je suis Développeur Full Stack",
+        home_desc: "Je conçois et développe des applications full stack, des interfaces modernes jusqu’aux systèmes back-end robustes.",
+
+        about_title: "À propos de moi",
+        about_desc: "Je suis un développeur Full Stack passionné par la création d’expériences numériques modernes, performantes et intuitives.",
+        about_dev:"Développeur Full Stack",
+
+        download_cv: "Télécharger le CV"
+    }
+};
+
+
+const langButtons = {
+    en: document.getElementById("lang-en"),
+    fr: document.getElementById("lang-fr")
+};
+
+function setLanguage(lang) {
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        const key = el.getAttribute("data-i18n");
+        if (translations[lang][key]) {
+            el.textContent = translations[lang][key];
+        }
+    });
+
+    localStorage.setItem("lang", lang);
+}
+
+langButtons.en.addEventListener("click", () => setLanguage("en"));
+langButtons.fr.addEventListener("click", () => setLanguage("fr"));
+
+/* Load saved language */
+const savedLang = localStorage.getItem("lang") || "en";
+setLanguage(savedLang);
+
+
+
+function setLanguage(lang) {
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        const key = el.getAttribute("data-i18n");
+        if (translations[lang][key]) {
+            el.textContent = translations[lang][key];
+        }
+    });
+
+    document.querySelectorAll(".lang-switch button")
+        .forEach(btn => btn.classList.remove("active"));
+
+    document.getElementById(`lang-${lang}`).classList.add("active");
+
+    localStorage.setItem("lang", lang);
+}
